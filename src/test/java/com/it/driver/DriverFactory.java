be25781d68d46d347c4f208620c02f10169b9b5e
andnemashkalo <alfonso8and@gmail.com>
2018-08-11 10:57:34 +0300

    test

--- src/test/java/com/it/driver/DriverFactory.java
+++ src/test/java/com/it/driver/DriverFactory.java
@@ -1,10 +1,20 @@
 package com.it.driver;
 
+import com.it.common.Constants;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DriverFactory {
-    public static WebDriver getDrive(){
-        System.out.println();
-        return null;
+    public static WebDriver getDriver() {
+        String property = System.getProperty("driver");
+        WebDriver driver;
+        if ("chrome".equals(property)) {
+            driver=new ChromeDriver();
+        } else {
+            driver=new ChromeDriver();
+        }
+        driver.manage().window().maximize();
+        driver.get(Constants.BASE_URL);
+        return driver;
     }
 }
--- src/test/java/com/it/pages/BasePage.java
+++ src/test/java/com/it/pages/BasePage.java
@@ -1,15 +1,11 @@
 package com.it.pages;
 
+import com.it.driver.DriverFactory;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.PageFactory;
 
 public abstract class BasePage {
-    protected static WebDriver driver;
-    static{ System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
-    driver = new ChromeDriver();
-    driver.get("http://www.i.ua/");
-    }
+    protected static WebDriver driver=DriverFactory.getDriver();
 
     public BasePage() {
         PageFactory.initElements(driver,this);
